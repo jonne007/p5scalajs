732056f2cbc6bcf2a26d2f9b61b88d10a75357c4
Magnus Larsson <magnus.larsson@tietoevry.com>
2022-03-14 20:36:02 +0100

    3d projection

--- js/src/Cube.scala
+++ js/src/Cube.scala
@@ -12,34 +12,36 @@ class SpinningCube() extends js.Object {
   val sketch: js.Function1[Sketch, Unit] = { s =>
     import s._
 
-    var cube = Cube(0, 0, 0, 50)
-    var angle = scala.util.Random.nextDouble
+    var cube = Cube(0, 0, 0)
+    var angle = 0.01
 
-    setup = () => createCanvas(200, 200)
-    //frameRate(1)
+    setup = () =>
+      createCanvas(500, 500)
 
     draw = () =>
       background(255)
       translate(width / 2, height / 2)
+      val rotation = rotationX(angle)
+        .matmult(rotationY(angle))
+        .matmult(rotationY(angle))
       val c = cube
-        .map(_.matmult(rotationX(angle)))
-        .map(_.matmult(rotationY(angle + 0.005)))
-        .map(_.matmult(rotationZ(angle + 0.007)))
+        .map(_.matmult(rotation))
+        .map(_.projected)
+        .map(_.mult(200))
 
       c.forConnections { (v1, v2) =>
         line(v1.x, v1.y, v2.x, v2.y)
       }
       c.forPoints { v =>
-        val z = map(v.z, -100, 100, 5, 7)
-        fill(199, 255, 43)
-        circle(v.x, v.y, z)
+        val d = map(v.z, -200, 200, 4, 7)
+        fill(0, 200, 0)
+        circle(v.x, v.y, d)
       }
-
-      angle = angle + 0.017
+      angle = angle + 0.01
   }
 }
 
-object Cube {
+object Cube:
   def apply(x: Double, y: Double, z: Double, r: Double = 0.5) =
     new Cube(
       Seq(
@@ -53,9 +55,8 @@ object Cube {
         Seq(-r, -r, r)
       )
     )
-}
 
-class Cube(points: Matrix) {
+class Cube(points: Matrix):
   def forPoints(f: MatrixVector => Unit) = points.foreach(f)
   def forConnections(f: (MatrixVector, MatrixVector) => Unit) =
     for (i <- 0 to 3) {
@@ -65,4 +66,3 @@ class Cube(points: Matrix) {
     }
 
   def map(f: Matrix => Matrix): Cube = new Cube(f(points))
-}
--- shared/src/matrix/Matrix.scala
+++ shared/src/matrix/Matrix.scala
@@ -5,9 +5,11 @@ type MatrixVector = Seq[Double]
 
 object MatrixVectorExt {
   extension (mv: MatrixVector)
-    def *(that: MatrixVector) = {
-      mv.zip(that).map(xy => xy._1 * xy._2).foldLeft(0.0)(_ + _)
-    }
+    def vecmult(that: MatrixVector) = mv
+      .zip(that)
+      .map { (x, y) => x * y }
+      .foldLeft(0.0)(_ + _)
+
     def x = mv(0)
     def y = mv(1)
     def z = mv(2)
@@ -17,16 +19,30 @@ object MatrixExt {
   extension (v: Matrix)
     def colVector(i: Int): MatrixVector = v.map(l => l(i))
     def rowVector(i: Int): MatrixVector = v(i)
+    def mult(n: Double): Matrix =
+      v.map(_.map(_ * n))
     def matmult(that: Matrix): Matrix = {
       import MatrixVectorExt._
       val l: Seq[Double] = for {
         row <- 0 until v.size
         col <- 0 until v(0).size
-      } yield v.rowVector(row) * that.colVector(col)
+      } yield v.rowVector(row) vecmult that.colVector(col)
       l.grouped(that(0).size).toSeq
     }
+    def projected: Matrix = {
+      import MatrixVectorExt._
+      val l: Seq[Double] = for {
+        row <- 0 until v.size
+        col <- 0 until v(0).size
+      } yield {
+        val rv = v.rowVector(row)
+        val p = Matrix.projection(rv.z)
+        p.colVector(col) vecmult rv
+      }
+      l.grouped(3).toSeq
+    }
 }
-object Matrix {
+object Matrix:
   def rotationX(angle: Double): Matrix =
     import Math._
     Seq(
@@ -46,13 +62,13 @@ object Matrix {
     Seq(
       Seq(cos(angle), -sin(angle), 0),
       Seq(sin(angle), cos(angle), 0),
-      Seq(0, 0, 1),
+      Seq(0, 0, 1)
     )
   def projection(z: Double): Matrix =
-    val zProj = 1 / (2 - z)
+    val distance = 3
+    val zProj = 1 / (distance - z)
     Seq(
       Seq(zProj, 0, 0),
       Seq(0, zProj, 0),
-      Seq(0, 0, 0)
+      Seq(0, 0, 1)
     )
-}
